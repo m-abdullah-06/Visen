fc5be5c1f0c1dd82238f64c937a01748c4a8d099
Muhammad Abdullah <abdullah.muhammad.xyz@gmail.com>
2025-11-22 11:02:00 +0500

    Resume Feedback Components

---
+++ app/component/scorebadge.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Scorebadge = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Scorebadge;
--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -55,7 +55,7 @@ const Resume = () => {
 
 
     return (
-        <main className="!pt-0">
+        <main className="!pt-0 bg-[url('/images/bg-main.svg')] bg-cover">
             <nav className="resume-nav">
                 <Link to="/" className="back-button">
                     <img src="/icons/back.svg" alt="logo" className="w-2.5 h-2.5" />
